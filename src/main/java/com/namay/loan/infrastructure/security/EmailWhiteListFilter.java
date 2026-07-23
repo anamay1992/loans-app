c8962c55cc1e022c4a08f3f38e774a5d3137dc05
anamay1992 <angel.n.cabanillas@gmail.com>
2026-07-22 22:09:23 -0500

    fix emails properties

--- src/main/java/com/namay/loan/infrastructure/security/EmailWhiteListFilter.java
+++ src/main/java/com/namay/loan/infrastructure/security/EmailWhiteListFilter.java
@@ -1,31 +1,32 @@
 package com.namay.loan.infrastructure.security;
 
+import io.quarkus.security.Authenticated;
 import jakarta.annotation.Priority;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.Priorities;
 import jakarta.ws.rs.core.Response;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eclipse.microprofile.jwt.JsonWebToken;
 import org.jboss.resteasy.reactive.server.ServerRequestFilter;
 
 import java.util.List;
 
 @Priority(Priorities.AUTHENTICATION + 10)
+@Authenticated
 public class EmailWhiteListFilter {
 
     @Inject
     JsonWebToken jwt;
 
-    private static final List<String> ALLOWED_EMAILS = List.of(
-            "angel.n.cabanillas@gmail.com",
-            "leslycamacho2018@gmail.com"
-    );
+    @ConfigProperty(name = "app.whitelist.emails")
+    List<String> allowedEmails;
 
     @ServerRequestFilter
     public Response filter() {
         if (jwt != null) {
             String email = jwt.getClaim("email");
             System.out.println(email);
-            if (email == null || !ALLOWED_EMAILS.contains(email)) {
+            if (email == null || !allowedEmails.contains(email)) {
                 return Response.status(Response.Status.FORBIDDEN)
                         .entity("{\"error\": \"Acceso denegado: Usuario no autorizado para este sistema.\"}")
                         .build();
